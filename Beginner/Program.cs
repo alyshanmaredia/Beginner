3f50f5468ad61da368285c926f7172767d22742f
alyshanmaredia <alyshanmaredia@gmail.com>
2020-12-12 02:42:33 +0500

    Add project files.

---
+++ Beginner/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "alishan";
+            for (int i = 0; i < name.Length; i++)
+            {
+
+            }
+            int[] arr = new int[2];
+   
+            int temp = name + 2;
+            char to = (char)temp;
+            Console.WriteLine("AI" + to);
+
+            Console.ReadLine();
+        }
+    }
+}
